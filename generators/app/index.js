22812f0e36617ce81e3bfd0aec91b9e3d5b6c2ee
Eded.Wang <wwhufo@gmail.com>
2017-05-09 23:46:55 +0800

    go

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,33 +6,96 @@ module.exports = class extends Generator {
 
   constructor(args, opts) {
     super(args, opts);
+  }
 
-    // 增加对 --eslint 的支持.
-    this.option('eslint');
+  before() {
+    this.log("开始创建项目~");
   }
 
   install() {
 
-    this.log('用户当前路径：' + this.contextRoot);
+    this.log("准备安装 package...");
 
-  }
+    var modules = [
+      "babel-core",
+      "babel-eslint",
+      "babel-loader",
+      "babel-preset-es2015",
+      "babel-preset-react",
+      "babel-preset-stage-2",
+      "css-loader",
+      "eslint",
+      "eslint-loader",
+      "eslint-plugin-react",
+      "extract-text-webpack-plugin",
+      "history",
+      "node-sass",
+      "react",
+      "react-addons-css-transition-group",
+      "react-dom",
+      "react-router",
+      "react-router-dom",
+      "react-transition-group",
+      "sass-loader",
+      "style-loader",
+      "webpack"
+    ];
 
-  method1() {
-    this.log('method 1 just ran');
-  }
+    this.npmInstall(modules, { 'save-dev': true });
 
-  method2() {
-    this.log('method 2 just ran');
+    this.log("结束安装 package...");
+
   }
 
   create() {
 
+    this.log("拷贝 [dist] ...");
+    this.fs.copy(
+      this.templatePath('dist'),
+      this.destinationPath('dist')
+    );
+    this.log("拷贝 [src] ...");
+    this.fs.copy(
+      this.templatePath('src'),
+      this.destinationPath('src')
+    );
+
+    this.log("拷贝 [.babelrc] ...");
+    this.fs.copy(
+      this.templatePath('.babelrc'),
+      this.destinationPath('.babelrc')
+    );
+    this.log("拷贝 [.eslintrc] ...");
+    this.fs.copy(
+      this.templatePath('.eslintrc'),
+      this.destinationPath('.eslintrc')
+    );
+    this.log("拷贝 [.gitignore] ...");
+    this.fs.copy(
+      this.templatePath('.gitignore'),
+      this.destinationPath('.gitignore')
+    );
+
+    this.log("拷贝 [webpack.config.js] ...");
+    this.fs.copy(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
+    );
+
+    /**
+     * fs 是 mem-fs-editor 实例
+     * @Link https://github.com/sboudrias/mem-fs-editor
+     */
     this.fs.copyTpl(
       this.templatePath('public/index.html'),
       this.destinationPath('public/index.html'),
       { title: 'Hello Templating with Yeoman' }
     );
 
+  }
+
+  end() {
+    this.log("项目创建完成！");
   }
 
 };
